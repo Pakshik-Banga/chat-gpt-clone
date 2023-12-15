6a6df23956cd25e2d6b694f62871fc6b3522aba4
Pakshik-Banga <pakshikbanga2004@gmail.com>
2023-12-15 20:17:20 +0530

    project created

---
+++ chat-gpt/src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+            {/* {console.log("Preet")} */}
+         <div className="upperSide">
+
+               <div className="upperSide_chatgpt_image">
+ 
+               {/* https://imgs.search.brave.com/r21fhm6Ks1zELUtURcaDYnOwAFwnnJ6w1h4LbqZAjes/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9mcmVl/bG9nb3BuZy5jb20v/aW1hZ2VzL2FsbF9p/bWcvMTY4MTAzODMy/NWNoYXRncHQtbG9n/by10cmFuc3BhcmVu/dC5wbmc */}
+
+                      {/* <img src="C:\Users\jk\Documents\Chat Gpt Clone\chat-gpt\src\chatGpt_logo_img.webp" alt="" className="logo" /> */}
+                   
+                      <span>ChatGPT</span>
+               </div>
+
+              {/* <button className="upperSide_midBtn"><img src="C:\Users\jk\Documents\Chat Gpt Clone\chat-gpt\src\chatGpt_logo_img.webp" alt="" className="upperSide_addBtn">New Chat</img></button> */}
+
+              <div className="upperSide_Bottom">
+
+                   <button className="upperSide_query"><img src={"C:\Users\jk\Documents\Chat Gpt Clone\chat-gpt\src\chatGpt_logo_img.webp"} alt="hello">What is Programming</img></button>
+                   
+              </div>
+
+         </div>
+
+         <div className="lowerSide">
+
+
+         </div>
+
+
+    </div>
+  );
+}
+
+export default App;
